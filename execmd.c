5191e9825222f7e1b7573b35285223cfe292335f
johnitopaisah <johnitopaisah@gmail.com>
2023-05-15 22:24:11 +0100

    function that handles the execution

--- execmd.c
+++ execmd.c
@@ -7,17 +7,23 @@
  */
 void execmd(char **argv)
 {
-	char *command = NULL;
+	char *command = NULL, *actual_command = NULL;
 
 	if (argv)
 	{
 		/* get the command */
 		command = argv[0];
 
+		/**
+		 * generating the path to this command before
+		 * passing it to the execve function
+		 */
+		actual_command = get_location(command);
+
 		/* execute the command with execve */
-		if (execve(command, argv, NULL) == -1)
+		if (execve(actual_command, argv, NULL) == -1)
 		{
 			perror("Error:");
-		};
+		}
 	}
 }
