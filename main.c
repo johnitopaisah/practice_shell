4318c5eb9fd2488e350b7991e5ce11e6d330efcb
johnitopaisah <johnitopaisah@gmail.com>
2023-05-17 18:32:45 +0100

    updated the main.c

--- main.c
+++ main.c
@@ -7,16 +7,22 @@ int main(void)
 {
 	char **argv;
 	char *lineptr, *lineptr_copy;
+	int is_input_tty = isatty(STDIN_FILENO);
 
 	/* Creating an infinite loop for the shell's prompt */
 	while (1 == 1)
 	{
-		start_prompt();
-
+		if (is_input_tty)
+		{
+			start_prompt();
+			fflush(stdout);
+		}
 		lineptr = line_command();
 		lineptr_copy = copy_func(lineptr);
 
 		argv = tok_parsing(lineptr, lineptr_copy);
+
+		handle_exit(argv);
 
 		execmd(argv);
 
